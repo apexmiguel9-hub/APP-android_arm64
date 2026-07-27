ce7f546eae834a751d2e380f8c1b2550f179eaac
root <root@localhost.localdomain>
2026-07-27 19:10:37 +0000

    fix: TextAlignment label missing composable in EditTextStyle.kt

--- OBlender/app/src/main/java/com/epai/oblender/ui/screens/main/control_editor/edit_widget/EditTextStyle.kt
+++ OBlender/app/src/main/java/com/epai/oblender/ui/screens/main/control_editor/edit_widget/EditTextStyle.kt
@@ -2,6 +2,7 @@ package com.epai.oblender.ui.screens.main.control_editor.edit_widget
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -23,7 +24,7 @@ fun EditTextStyle(data: ObservableWidget, onEditWidgetText: (ObservableTranslata
 
 private fun LazyListScope.commonItems(onEditWidgetText: () -> Unit, textAlignment: TextAlignment, onTextAlignmentChanged: (TextAlignment) -> Unit, textBold: Boolean, onTextBoldChanged: (Boolean) -> Unit, textItalic: Boolean, onTextItalicChanged: (Boolean) -> Unit, textUnderline: Boolean, onTextUnderlineChanged: (Boolean) -> Unit) {
     item { InfoLayoutTextItem(title = stringResource(R.string.control_editor_edit_text), onClick = onEditWidgetText) }
-    item { InfoLayoutSelectItem(title = stringResource(R.string.control_editor_edit_text_alignment), options = TextAlignment.entries, current = textAlignment, onClick = { if (textAlignment != it) onTextAlignmentChanged(it) }, label = {}) }
+    item { InfoLayoutSelectItem(title = stringResource(R.string.control_editor_edit_text_alignment), options = TextAlignment.entries, current = textAlignment, onClick = { if (textAlignment != it) onTextAlignmentChanged(it) }, label = { Text(it.name) }) }
     item { InfoLayoutSwitchItem(title = stringResource(R.string.control_editor_edit_text_bold), value = textBold, onValueChange = onTextBoldChanged) }
     item { InfoLayoutSwitchItem(title = stringResource(R.string.control_editor_edit_text_italic), value = textItalic, onValueChange = onTextItalicChanged) }
     item { InfoLayoutSwitchItem(title = stringResource(R.string.control_editor_edit_text_underline), value = textUnderline, onValueChange = onTextUnderlineChanged) }
