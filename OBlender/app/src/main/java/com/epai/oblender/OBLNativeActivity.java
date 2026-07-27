2f338e508374fbf058c76fe2542b73e9b2184740
root <root@localhost.localdomain>
2026-07-27 03:46:28 +0000

    fix: 🎮 add PixelFormat.TRANSPARENT for overlay window surface

--- OBlender/app/src/main/java/com/epai/oblender/OBLNativeActivity.java
+++ OBlender/app/src/main/java/com/epai/oblender/OBLNativeActivity.java
@@ -494,6 +494,7 @@ public class OBLNativeActivity extends NativeActivity
                     lp.flags = LayoutParams.FLAG_NOT_FOCUSABLE;
                     lp.flags |= LayoutParams.FLAG_LAYOUT_IN_SCREEN;
                     lp.flags |= LayoutParams.FLAG_NOT_TOUCH_MODAL;
+                    lp.format = PixelFormat.TRANSPARENT;
                     lp.gravity = Gravity.CENTER;
                     lp.width = (int)(getResources().getDisplayMetrics().widthPixels * 0.9);
                     lp.height = (int)(getResources().getDisplayMetrics().heightPixels * 0.9);
