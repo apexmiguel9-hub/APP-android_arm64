be1e86a4aaf41168ed847f614df007371a252ac8
root <root@localhost.localdomain>
2026-07-30 03:13:19 +0000

    fix: restore missing import compose.runtime.*

--- OBlender/app/src/main/java/com/epai/oblender/VirtualPointerOverlay.kt
+++ OBlender/app/src/main/java/com/epai/oblender/VirtualPointerOverlay.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.*
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
